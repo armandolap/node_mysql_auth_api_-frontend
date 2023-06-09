57e47170abfd4c4b1c749965cf8f37a287ec33ef
armandolap <armando.lap@gmail.com>
2023-06-09 16:26:44 +0200

    090623

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -14,16 +14,19 @@ lis.forEach(li => {
       case '1':
         formTitle.innerHTML = "Registro";
         form.classList.add('register');
+        form.setAttribute('fetchMethod', 'post');
       break;
       case '2':
         formTitle.innerHTML = "Login";
         form.classList.add('authenticate');
+        form.setAttribute('fetchMethod', 'post');
       break;
       case '3':
         formTitle.innerHTML = "Ver mi usuario";
         inputs[0].classList.add('d-none');
         inputs[1].classList.add('d-none');
         form.classList.add('current');
+        form.setAttribute('fetchMethod', 'get');
       break;
     }
   });
@@ -32,23 +35,44 @@ lis.forEach(li => {
 // form submitted and fetch 
 form.addEventListener('submit', e => {
   e.preventDefault();
+  console.log(validateForm());
   if (validateForm()) {
+    const apiOption = e.target.className;
+    const fetchMethod = form.getAttribute('fetchMethod');
+    let fetchOptions = {};
     // fetch opcions obj
-    const fetchOptions = {
-      method: 'post',
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ 
-        username: inputs[0].value, 
-        password: inputs[1].value 
-      })
+    switch (fetchMethod) {
+      case 'post':
+        fetchOptions = {
+          method: 'post',
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ 
+            username: inputs[0].value, 
+            password: inputs[1].value 
+          })
+        }
+        break;
+      case 'get': 
+        const token = localStorage.getItem("token");
+        fetchOptions = {
+          method: 'get',
+          headers: { 
+            "Content-Type": "application/json", 
+            "Authorization": "Bearer " + token
+          },
+        }
     }
-    fetch('http://127.0.0.1:3000/users/' + e.target.className, fetchOptions)
+    
+    fetch('http://127.0.0.1:3000/users/' + apiOption, fetchOptions)
     .then(res => res.json())
     .then(data => {
       if (data.token) {
         msgDiv.innerHTML = "Login correcto. <br><br> Token: " + data.token;
-      } else {
+        localStorage.setItem("token", data.token);
+      } else if (data.message) {
         msgDiv.innerHTML = data.message;
+      } else {
+        msgDiv.innerHTML = `id: ${data.id}, username: ${data.username}`; 
       }
     })
     .catch( error => {
@@ -60,21 +84,22 @@ form.addEventListener('submit', e => {
 // reset form
 function resetForm() {
   form.reset();
-  form.className = '';
+  form.className, msgDiv.innerHTML = '';
   inputs.forEach(input => {
     input.classList.remove('borderValidation');
+    input.classList.remove('d-none');
   });
 }
 
 // validate form
 function validateForm() {
   inputs.forEach(input => {
-    if (input.value.length === 0) {
+    if (input.value.length === 0) { 
       input.classList.add('borderValidation');
       return false;
     } else {
       input.classList.remove('borderValidation');
     }
   });
-  return true;
+ // return true;
 }
